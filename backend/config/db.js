9febd2a938c895ee22bfcc62649191ec395b7732
Mickie <mickystn10@gmail.com>
2023-06-16 19:22:50 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection('mysql://pz1gr10qi50g8n16i1i5:pscale_pw_fkHnaqPycoSUkcaiu63LUiu04Q4qjclBCZSXLtVDydb@aws.connect.psdb.cloud/db-meetingroom?ssl={"rejectUnauthorized":false}'
+const db = mysql.createConnection('mysql://pz1gr10qi50g8n16i1i5:pscale_pw_fkHnaqPycoSUkcaiu63LUiu04Q4qjclBCZSXLtVDydb@aws.connect.psdb.cloud/db-meetingroom?ssl={"rejectUnauthorized":true}'
 );
 
 module.exports = db;
