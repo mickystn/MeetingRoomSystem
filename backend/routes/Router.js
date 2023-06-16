4a19d4bde5e895f287b26326c43714bee2a019d9
Mickie <mickystn10@gmail.com>
2023-06-16 19:12:26 +0700

    Update Router.js

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -6,7 +6,7 @@ const {getBooking,booking,deleteBooking} = require('../controller/room');
 
 
 
-router.get("/",getBooking)
+router.get("/test",getBooking)
 router.post("/booking",booking)
 router.delete("/deleteBooking/:user_email",deleteBooking)
 
