6f7deb9588cf22f4d98fdb6697cabe2010d387f7
Mickie <mickystn10@gmail.com>
2023-06-16 21:53:19 +0700

    database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    user:"admin",
-    host: "meetingroom.c529p3llzbxh.us-east-1.rds.amazonaws.com",
-    password: "Mannmixx2110!",
-    database: "meetingroom",
+    user:process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 module.exports = db;
